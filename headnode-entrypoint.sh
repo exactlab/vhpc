9fdda305fa263f15aa6d3e5855d4a69ad5c0973a
Alberto Pastorutti <6527012+apasto@users.noreply.github.com>
2025-09-01 12:08:57 +0200

    headnode entrypoint: copy config from /var/slurm_config

--- headnode-entrypoint.sh
+++ headnode-entrypoint.sh
@@ -2,6 +2,12 @@
 set -e
 
 echo "Initializing shared slurm configuration..."
+# Copy any files in /var/slurm_config to /etc/slurm
+# This is meant to provide a configuration source from the host (via a bind mount)
+# while never writing/chowning files on the host
+if [ -d "/var/slurm_config" ]; then
+    cp -r /var/slurm_config/. /etc/slurm/
+fi
 # Set proper ownership for shared SLURM configs (mounted from ./slurm-config/)
 # This is required because Docker volumes are owned by root by default
 chown -R slurm:slurm /etc/slurm
